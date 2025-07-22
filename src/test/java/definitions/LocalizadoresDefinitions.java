03c1edd3a59536a015fff97a26aa0e903bca69a5
Gerardo Cardoso <ger9047@gmail.com>
2025-07-22 17:47:50 -0600

    Localizadores

--- src/test/java/definitions/LocalizadoresDefinitions.java
+++ src/test/java/definitions/LocalizadoresDefinitions.java
@@ -34,26 +34,32 @@ public class LocalizadoresDefinitions {
         //otra forma pero le tenemos que agregar otra linea
         //WebElement inputNombre = driver.findElement(By.id("user-name"));
         //inputNombre.sedKeys(userName);
-        driver.findElement(By.id("user-name")).sendKeys(userName);
+        //driver.findElement(By.id("user-name")).sendKeys(userName);
+        //Por xpath
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys(userName);
+
     }
 
     @And("ingreso {string} en el campo de password")
     public void ingreso_en_el_campo_de_password(String password) throws InterruptedException {
         Thread.sleep(3000);
-        driver.findElement(By.name("password")).sendKeys(password);
+        //driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@data-test='password']")).sendKeys(password);
     }
 
     @And("presiono el boton login")
     public void presiono_el_boton_login() throws InterruptedException {
         Thread.sleep(3000);
-        driver.findElement(By.className("submit-button")).click();
+        //driver.findElement(By.className("submit-button")).click();
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
         Thread.sleep(5000);
     }
 
     @Then("valido que me haya logueado en el sitio")
     public void valido_que_me_haya_logueado_en_el_sitio() throws InterruptedException {
         Thread.sleep(3000);
-        Assert.assertTrue("No se ha realizado el login", driver.findElement(By.xpath("//span[contains(text(),'Products_1')]")).isDisplayed());
+        Assert.assertTrue("No se ha realizado el login", driver.findElement(By.xpath("//span[contains(text(),'Products')]")).isDisplayed());
+        //Assert.assertTrue("No se ha realizado el login", driver.findElement(By.xpath("//input[@data-test='Login']")).isDisplayed());
         Thread.sleep(3000);
     }
 
